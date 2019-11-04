e8c096f3c9274c65e2feef98533777bc40ebec7e
Morozova Aleksandra <sashamorozova01@mail.ru>
2019-11-05 00:13:17 +0300

    Initial commit

---
+++ desktop/src/ru/realsanya/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.realsanya.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.realsanya.snake.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
