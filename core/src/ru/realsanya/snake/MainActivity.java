df02883536e1509c5480df722bee1e55bb560d80
Morozova Aleksandra <sashamorozova01@mail.ru>
2019-11-10 16:43:55 +0300

    07

--- core/src/ru/realsanya/snake/MainActivity.java
+++ core/src/ru/realsanya/snake/MainActivity.java
@@ -11,7 +11,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import javafx.stage.Stage;
 
 public class MainActivity extends Game {
 
@@ -21,15 +24,18 @@ public class MainActivity extends Game {
     Snake snake;
     Food food;
 
+    Stage stage;
+    StretchViewport viewport;
+
 
 
     public void create() {
+        viewport = new StretchViewport(640, 360);
+        stage = new Stage();
+
         batch = new SpriteBatch();
         snake = new Snake();
-
         food = new Food();
-
-
     }
 
     public void update() {
@@ -42,10 +48,10 @@ public class MainActivity extends Game {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
          food.render(batch);
-            if ((snake.getX() >= food.getX()) &&
-                    (snake.getX() + snake.getWidth()) <= (food.getX() + food.getWidth())
-                    && (snake.getY() >= food.getY()) &&
-                    (snake.getY() + snake.getHeight() <= food.getY() + food.getHeight())) {
+            if ((snake.x >= food.x) &&
+                    (snake.x + snake.getWidth()) <= (food.x + food.getWidth())
+                    && (snake.y >= food.y) &&
+                    (snake.y + snake.getHeight() <= food.y + food.getHeight())) {
                 update( );
             }
         snake.render(batch);
