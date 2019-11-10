a9903f4bd299d6f96f46a12460e83aa2b5b9652c
Morozova Aleksandra <sashamorozova01@mail.ru>
2019-11-10 17:50:25 +0300

    07

--- core/src/ru/realsanya/snake/MainActivity.java
+++ core/src/ru/realsanya/snake/MainActivity.java
@@ -4,17 +4,9 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-
-import javafx.stage.Stage;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainActivity extends Game {
 
@@ -24,17 +16,22 @@ public class MainActivity extends Game {
     Snake snake;
     Food food;
 
+
+
     Stage stage;
     StretchViewport viewport;
 
 
     public void create() {
         viewport = new StretchViewport(640, 360);
-        stage = new Stage();
+        stage = new Stage(viewport);
 
         batch = new SpriteBatch();
         snake = new Snake();
         food = new Food();
+
+       // stage.addActor(food);
+
     }
 
     public void update() {
@@ -42,7 +39,7 @@ public class MainActivity extends Game {
     }
 
 
-    public void render(SpriteBatch batch) {
+    public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
